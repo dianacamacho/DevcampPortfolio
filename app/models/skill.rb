d54ee1f4372e440d47086f54bc85cc85c0bdb5a7
dianacamacho <dcam1901@yahoo.com>
2017-04-14 02:42:36 -0400

    default for skill images reusing portfolio set_default code

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,9 @@
 class Skill < ApplicationRecord
   validates :title, :percent_utilized, presence: true
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.badge ||= 'http://placehold.it/250x250'
+  end
 end
